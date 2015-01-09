8cf6653d3745ea9ddf950e7a323241966230fa5f
cavis <ryancavis@gmail.com>
2015-01-09 13:23:04 -0700

    Allow for doc files without h1's in them

--- spec/features/docs_spec.rb
+++ spec/features/docs_spec.rb
@@ -3,9 +3,19 @@ require 'rails_helper'
 feature 'api documentation' do
 
   let(:markdown_titles) do
+    non_null_titles = []
     Dir.glob("#{Rails.root}/docs/*.md").map do |file|
-      IO.read(file)[/^#.+$/].gsub(/^#/, '').strip
+      txt = IO.read(file)
+      hdr = txt[/^#[^#].+$/]
+
+      # must occur before first code block
+      if hdr
+        if !txt.index('```') || txt.index(hdr) < txt.index('```')
+          non_null_titles << hdr.gsub(/^#/, '').strip
+        end
+      end
     end
+    non_null_titles
   end
 
   scenario 'user goes to the docs page' do
