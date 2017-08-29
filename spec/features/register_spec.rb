0cf481e58f4e29352d51cccff1a73761b5a14b61
Alberto Vasquez <alberto.vasquez@gmail.com>
2017-08-29 13:39:44 -0400

    Remove tests that revolve submitting forms that are no longer operational.

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -7,45 +7,45 @@ feature 'user registration' do
   let(:first_mail) { ActionMailer::Base.deliveries.first }
   before(:each)    { ActionMailer::Base.deliveries.clear }
 
-  scenario 'fails to fill out all fields' do
-    visit register_path
-    click_button 'Send request'
-    expect(page).to have_content('Please fill out all fields')
-  end
+#  scenario 'fails to fill out all fields' do
+#    visit register_path
+#    click_button 'Send request'
+#    expect(page).to have_content('Please fill out all fields')
+#  end
 
-  scenario 'sets a fancy reverse captcha field' do
-    visit register_path
-    set_host_picker 'https://api.pmp.io'
-    fill_in 'Your name', with: 'Foo Bar'
-    fill_in 'Contact email', with: 'foo@bar.com'
-    fill_in 'Name of organization', with: 'Foobar Inc'
-    set_cms_picker ['Drupal', 'Wordpress']
-    fill_in 'For what purpose do you intend to use the PMP?', with: 'Profit'
-    page.find('#email').set('foobar')
-    click_button 'Send request'
-    expect(page).to have_content('Hidden form fields were submitted')
-  end
+#  scenario 'sets a fancy reverse captcha field' do
+#    visit register_path
+#    set_host_picker 'https://api.pmp.io'
+#    fill_in 'Your name', with: 'Foo Bar'
+#    fill_in 'Contact email', with: 'foo@bar.com'
+#    fill_in 'Name of organization', with: 'Foobar Inc'
+#    set_cms_picker ['Drupal', 'Wordpress']
+#    fill_in 'For what purpose do you intend to use the PMP?', with: 'Profit'
+#    page.find('#email').set('foobar')
+#    click_button 'Send request'
+#    expect(page).to have_content('Hidden form fields were submitted')
+#  end
 
-  scenario 'successful registration' do
-    visit register_path
-    set_host_picker 'https://api.pmp.io'
-    fill_in 'Your name', with: 'Foo Bar'
-    fill_in 'Contact email', with: 'foo@bar.com'
-    fill_in 'Name of organization', with: 'Foobar Inc'
-    set_cms_picker ['Drupal', 'Wordpress']
-    fill_in 'For what purpose do you intend to use the PMP?', with: 'Profit'
-    click_button 'Send request'
-    expect(page).to have_content('your request has been sent')
-    expect(current_path).to eq(login_path)
-
-    expect(mails.count).to eq(1)
-    expect(first_mail.to).to include('support@publicmediaplatform.org')
-    expect(first_mail.subject).to eq('PMP registration request from Foo Bar')
-    expect(first_mail.body).to include('production')
-    expect(first_mail.body).to include('foo@bar.com')
-    expect(first_mail.body).to include('Foobar Inc')
-    expect(first_mail.body).to include('Drupal, Wordpress')
-    expect(first_mail.body).to include('Profit')
-  end
+#  scenario 'successful registration' do
+#    visit register_path
+#    set_host_picker 'https://api.pmp.io'
+#    fill_in 'Your name', with: 'Foo Bar'
+#    fill_in 'Contact email', with: 'foo@bar.com'
+#    fill_in 'Name of organization', with: 'Foobar Inc'
+#    set_cms_picker ['Drupal', 'Wordpress']
+#    fill_in 'For what purpose do you intend to use the PMP?', with: 'Profit'
+#    click_button 'Send request'
+#    expect(page).to have_content('your request has been sent')
+#    expect(current_path).to eq(login_path)
+#
+#    expect(mails.count).to eq(1)
+#    expect(first_mail.to).to include('support@publicmediaplatform.org')
+#    expect(first_mail.subject).to eq('PMP registration request from Foo Bar')
+#    expect(first_mail.body).to include('production')
+#    expect(first_mail.body).to include('foo@bar.com')
+#    expect(first_mail.body).to include('Foobar Inc')
+#    expect(first_mail.body).to include('Drupal, Wordpress')
+#    expect(first_mail.body).to include('Profit')
+#  end
 
 end
